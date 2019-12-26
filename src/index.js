b67fd727eef925a0d356b7c426a2498e5944cad6
OBKoro1 <1677593011@qq.com>
2019-12-26 17:58:47 +0800

    Revert "vscode 插件初始化"
    This reverts commit fede0d08369f01950f61af88883c29f76e56d273.

--- src/index.js
+++ src/index.js
@@ -2,96 +2,91 @@
  * Author       : OBKoro1
  * Date         : 2019-12-19 20:23:57
  * LastEditors  : OBKoro1
- * LastEditTime : 2019-12-25 15:23:38
- * FilePath     : /autoCommit/src/index.js
+ * LastEditTime : 2019-12-23 19:56:54
+ * FilePath     : /autoCommit/index.js
  * Description  : 自动commit
  * https://github.com/OBKoro1
  */
 
-const { execSync } = require('child_process')
-const moment = require('moment')
-const fs = require('fs')
+const { execSync } = require('child_process');
+const moment = require('moment');
+const fs = require('fs');
 
 class autoCommit {
-  constructor () {
-    this.init()
+  constructor() {
+    this.init();
   }
-  getData () {
-    this.getAllDay(this.paramsObj.beginDay, this.paramsObj.endDay)
-    this.readyCommit()
+  getData() {
+    this.getAllDay(this.paramsObj.beginDay, this.paramsObj.endDay);
+    this.readyCommit();
+    console.log('timeArr', this.timeArr);
   }
-  // 格式化日期
-  formatTime (time) {
-    return `${time} 08:00`
+  formatTime(time) {
+    return `${time} 08:00`;
   }
-  // 获取两个日期之间的间隔: [ '2019-02-02', '2019-02-03' ... ]
-  getAllDay (begin, end) {
-    this.timeArr = []
-    const beginSplit = begin.split('-')
-    const endSplit = end.split('-')
-    const beginDate = new Date()
-    beginDate.setUTCFullYear(beginSplit[0], beginSplit[1] - 1, beginSplit[2])
-    const endDate = new Date()
-    endDate.setUTCFullYear(endSplit[0], endSplit[1] - 1, endSplit[2])
-    const beginNumber = beginDate.getTime()
-    const endNumber = endDate.getTime()
-    for (let k = beginNumber; k <= endNumber;) {
-      const day = new Date(parseInt(k))
-      const dayFormat = moment(day).format('YYYY-MM-DD')
-      this.timeArr.push(dayFormat)
-      k = k + 24 * 60 * 60 * 1000
+  getAllDay(begin, end) {
+    this.timeArr = [];
+    const beginSplit = begin.split('-');
+    const endSplit = end.split('-');
+    const beginDate = new Date();
+    beginDate.setUTCFullYear(beginSplit[0], beginSplit[1] - 1, beginSplit[2]);
+    const endDate = new Date();
+    endDate.setUTCFullYear(endSplit[0], endSplit[1] - 1, endSplit[2]);
+    const beginNumber = beginDate.getTime();
+    const endNumber = endDate.getTime();
+    for (let k = beginNumber; k <= endNumber; ) {
+      const day = new Date(parseInt(k));
+      const dayFormat = moment(day).format('YYYY-MM-DD');
+      this.timeArr.push(dayFormat);
+      k = k + 24 * 60 * 60 * 1000;
     }
   }
-  init () {
-    // TODO: 获取参数
+  // 只commit今天
+  commitToday() {
+    const time = moment().format('DD/MM/YYYY HH:MM:ss');
+    // TODO:
+  }
+  init() {
     let paramsObj = {
-      beginDay: '2019-12-20',
-      itemSrc: '/Users/koro/work/web_my/testCommit' // 要commit的项目地址
-    }
-    let defaultOption = {
-      beginDay: moment().format('YYYY-MM-DD'), // 默认为今天
-      endDay: moment().format('YYYY-MM-DD'), // 默认为今天
-      fileSrc: `commit.md`,
+      beginDay: '2019-02-02',
+      endDay: '2019-03-02',
+      itemSrc: '../../testCommit', // 要commit的项目地址
       commitNumber: 1 // 每天commit 次数
-    }
-    this.paramsObj = Object.assign(defaultOption, paramsObj)
-    this.getData()
+    };
+    let defaultOption = {
+      endDay: moment().format('YYYY-MM-DD'), // 默认为今天
+      fileSrc: `${paramsObj.itemSrc}/commit.md`
+    };
+    this.paramsObj = paramsObj;
+    this.getData();
+    // this.commit();
   }
-  // commit
-  readyCommit () {
-    console.log('日期数组:', this.timeArr)
-    console.log('每个日期提交次数:', this.paramsObj.commitNumber)
-    let totalNum = 0 // 总commit次数
+  readyCommit() {
     // 遍历日期
     this.timeArr.forEach(item => {
       // 每个日期commit次数
-      for (let i = 0; i < this.paramsObj.commitNumber; i++) {
-        let time = this.formatTime(item) // 2019-01-02 08:00
-        time = moment(time).format() // 2019-01-02T00:00:00+0800
-        const commitContent = `${time}${i}`
+      for (let i = 0; i++; i < this.paramsObj.commitNumber) {
+        let time = this.formatTime(item); // 2019-01-02 08:00
+        time = moment(time).format(); // 2019-01-02T00:00:00+0800
         fs.writeFileSync(
-          `${this.paramsObj.itemSrc}/${this.paramsObj.fileSrc}`,
-          commitContent,
+          `${this.paramsObj.itemSrc}/commit.md`,
+          `${time}${i}`,
           'utf-8'
-        )
-        this.myExecSync(
-          `cd ${this.paramsObj.itemSrc} && git add . && git commit -m 'autoCommit' --date='${time}' && git pull && git push origin master`
-        )
-        let cmd = `git log -1 \
-        --date=iso --pretty=format:'{"commit": "%h","author": "%aN <%aE>","date": "%ad","message": "%s"},' \
-        $@ | \
-        perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
-        perl -pe 's/},]/}]/'`
-        this.myExecSync(cmd)
-        this.totalNum++
-
-        console.log('commit内容', commitContent)
-        console.log(`总commit次数${totalNum}`)
+        );
+        console.log('each', time);
       }
-    })
+      // this.commit()
+    });
+  }
+  commit(commitTime) {
+    // git commit --amend --date="2019-01-02T00:00:00+0800" -am 'autoCommit'
+    // TODO: cd 项目 重写文件和commit
+    this.myExecSync(
+      `cd ${this.paramsObj.itemSrc} && git add . && git commit -m 'autoCommit' --date='${commitTime}' && git pull && git push origin master`
+    );
   }
 
-  myExecSync (cmd) {
+  myExecSync(cmd) {
     // 除了该方法直到子进程完全关闭后才返回 执行完毕 返回
     try {
       const res = execSync(cmd, {
@@ -101,12 +96,12 @@ class autoCommit {
         killSignal: 'SIGTERM',
         cwd: undefined,
         env: undefined
-      })
-      return res
+      });
+      return res;
     } catch (err) {
-      console.log(`执行命令出错:${cmd}`)
+      console.log(`执行命令出错:${cmd}`);
     }
   }
 }
 
-new autoCommit()
+new autoCommit();
