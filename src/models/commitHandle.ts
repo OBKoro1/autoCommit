abe2349cae0290ec8aa8af4b1d6d462ebce7ba16
OBKoro1 <1677593011@qq.com>
2020-01-04 14:12:29 +0800

    issue

--- src/models/commitHandle.ts
+++ src/models/commitHandle.ts
@@ -2,7 +2,7 @@
  * Author       : OBKoro1
  * Date         : 2019-12-30 16:59:30
  * LastEditors  : OBKoro1
- * LastEditTime : 2020-01-02 22:15:59
+ * LastEditTime : 2020-01-04 11:14:22
  * FilePath     : /autoCommit/src/models/commitHandle.ts
  * Description  : commit 具体操作
  * https://github.com/OBKoro1
@@ -114,7 +114,7 @@ class CommitHandle {
           try {
             // 异步执行命令 让出线程 打印日志 等
             commitMsg = await new Promise((resolve, reject) => {
-              let cmd = `cd ${this.paramsObj.itemSrc} && git add . && git commit -m 'autoCommit' --date='${time}' && git pull && git push`;
+              let cmd = `cd ${this.paramsObj.itemSrc} && git add . && git commit -m '${this.paramsObj.commitMsg}' --date='${time}' && git pull && git push`;
               exec(cmd, (error, stdout, stderr) => {
                 if (error) {
                   outputLog(`执行命令出错:${cmd}`);
